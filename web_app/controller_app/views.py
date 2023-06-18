d1f2f599abdfa5ecbf590fed13ae51b39c44d73f
ShivamChaurasia01 <118128276+ShivamChaurasia01@users.noreply.github.com>
2023-06-18 15:15:26 +0530

    Change Login Password

--- web_app/controller_app/views.py
+++ web_app/controller_app/views.py
@@ -4,11 +4,31 @@ from django.views.generic.base import View
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        old_password = self.request.POST.get('old_password', None)
+        new_password = self.request.POST.get('new_password', None)
+        request.user.set_password('new password')
+        request.user.save()
+        messages.success(request, 'Your password has been successfully changed.')
+
+    return render(request, 'body.html', {'form': form})
+
+
 
 class LoginPage(TemplateView):
     template_name = 'index.html'
@@ -39,6 +59,29 @@ class LoginPage(TemplateView):
         context['new_variable'] = 'new_variable' + ' updated'
 
         return self.render_to_response(context)
+
+
+class ProfilePage(LoginRequiredMixin, TemplateView):
+    template_name = 'profile.html'
+    login_url = "/"
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+
+        old_password = self.request.POST.get('old_password', None)
+        new_password = self.request.POST.get('new_password', None)
+        if request.user.check_password(old_password):
+            request.user.set_password(new_password)
+            request.user.save()
+
+            messages.success(request, 'Your password has been successfully changed.')
+        else:
+            messages.warning(request, 'Your old password is incorrect.')
+        return self.render_to_response(context)
+    def get_context_data(self, **kwargs):
+        kwargs = super(ProfilePage, self).get_context_data(**kwargs)
+        kwargs["form"] = PasswordChangeForm(user=self.request.user)
+        return kwargs
 
 
 
--- web_app/web_app/urls.py
+++ web_app/web_app/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.LoginPage.as_view(), name="login"),
     path('dashboard', views.DashboardPage.as_view(), name="login"),
+    path('profile', views.ProfilePage.as_view(), name="profile"),
     path('logout', views.logout_view, name="logout"),
+    path('change_password/', views.change_password, name='change_password'),
 ]
